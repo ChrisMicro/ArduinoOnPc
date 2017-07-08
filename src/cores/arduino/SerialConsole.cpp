a46e8936dfeb3d228761168c581c625955fe6ded
ChrisMicro <christoph@roboterclub-freiburg.de>
2017-07-08 19:21:37 +0200

    Serial added

---
+++ src/cores/arduino/SerialConsole.cpp
@@ -0,0 +1,50 @@
+/*
+ * SerialConsole.cpp
+ *
+ *  Created on: 08.07.2017
+ *      Author: christoph
+ */
+
+#include "SerialConsole.h"
+
+SerialConsole::SerialConsole()
+{
+
+}
+
+void SerialConsole:: begin(const uint32_t baud)
+{
+
+}
+
+void SerialConsole::end(void)
+{
+
+}
+
+int SerialConsole::available(void)
+{
+	return 0;
+}
+
+int SerialConsole::peek(void)
+{
+	return 0;
+}
+
+int SerialConsole::read(void)
+{
+	return 0;
+}
+
+void SerialConsole::flush(void)
+{
+
+}
+
+size_t SerialConsole::write(const uint8_t c)
+{
+	putchar(c);
+	return 1;
+}
+
