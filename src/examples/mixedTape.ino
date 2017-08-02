4114e9e31e4c34f55cc8407628913d5fe5227c9d
ChrisMicro <christoph@roboterclub-freiburg.de>
2017-08-02 07:21:24 +0200

    mixedTape function beautified

--- src/examples/mixedTape.ino
+++ src/examples/mixedTape.ino
@@ -9,12 +9,14 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMBEROFLEDS,tft);
 
-GUI_Led   led1(20,220,"p");
-GUI_Led   led2(380,220,"o");
+GUI_Led    led1(20,220,"p");
+GUI_Led    led2(380,220,"o");
 GUI_Button buttonLeft(120,180,"l");
 GUI_Button buttonRight(310,180,"r");
 GUI_Slider potiLeft(10,10,"l");
 GUI_Slider potiRight(400,10,"r");
+
+TouchScreen ts;
 
 void setup()
 {
@@ -25,24 +27,58 @@ void setup()
   buttonRight.show();
   potiLeft.show();
   potiRight.show();
+  led1.on();
+}
+
+void shiftStripe()
+{
+	for(int n=NUMBEROFLEDS-1;n>-1;n--)
+	{
+		uint32_t color=strip.getPixelColor(n);
+		strip.setPixelColor(n+1,color);
+	}
 }
 
 int Counter=0;
 
 void loop(void)
 {
-  led1.on();
-  delay(400);
-  led1.off();
-  delay(400);
-  strip.setPixelColor(Counter++,0,0,0);
-  strip.show();
-  // we can blink easier:
-  led2.toggle();
+  // read touch pad and tell the gui
+  TSPoint p = ts.getPoint();
+  gui.setTouch(p.x, p.y, p.z);
 
-  strip.setPixelColor(Counter,0,0,255);
-  strip.show();
-  if(Counter>NUMBEROFLEDS)Counter=0;
+  int value1 = potiLeft.getInt();
+  int value2 = potiRight.getInt();
+
+
+  static int r=0;
+  static int g=0;
+  static int b=0;
+
+  if(buttonLeft.wasPressed()) r=255;
+
+  if(buttonRight.wasPressed())g=255;
+
+  Counter++;
+  if(Counter>=value1)
+  {
+	  Counter=0;
+	  led2.toggle();
+
+	  if(r==0 && g==0 )
+	  {
+		  r=0;
+		  g=value2*2;
+		  b=255-value2*2;
+	  }
+	  strip.setPixelColor(0,r,g,b);
+	  shiftStripe();
+	  strip.show();
+	  r=g=b=0;
+  }
+
+  delay(10);
+
 }
 
 
