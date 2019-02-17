5217a2bb55e79ffbe68c405bc84aa005de4d13ea
ChrisMicro <christoph@roboterclub-freiburg.de>
2019-02-17 04:32:30 +0100

    guiLed example simplified

--- examples/guiLed/guiLed.ino
+++ examples/guiLed/guiLed.ino
@@ -1,36 +1,7 @@
 #include "TFT_LinuxWrapper.h"
-//#include "../libraries/Wire/Wire.h"
+
 #define TFT_PC
 #include "../libraries/GuiPittix/Gui.h"
-
-//TFT_LinuxWrapper tft;
-
-/*
-void setup()
-{
-  Serial.begin(115200);
-  Serial.print("test");
-  tft.begin();
-  tft.fillScreen(LTDC_BLACK);
-
-  //tft.setRotation(1);
-  tft.setCursor(0, 0);
-  tft.setTextColor(LTDC_GREEN);  tft.setTextSize(3);
-  tft.println("Arduino TFT emulation on PC");
-  tft.setTextColor(LTDC_YELLOW); tft.setTextSize(2);
-}
-
-int Counter=0;
-
-void loop(void)
-{
-
-  tft.print( Counter ); tft.print(" ");
-  Counter++;
-  analogWrite(1,Counter);
-  delay(1000);
-  Serial.println(Counter);
-}*/
 
 GUI_Led   led1("set   ");
 GUI_Led   led2("toggle");
@@ -38,16 +9,16 @@ GUI_Led   led2("toggle");
 void setup()
 {
   gui.begin();
+  led1.off();
+  led2.off();
 }
 
 void loop(void)
 {
+  delay(1000);
   led1.on();
-  delay(1000);
-  led1.off();
-  delay(1000);
 
-  // we can blink easier:
+  // we can blink easily with the toggle method
   led2.toggle();
 }
 
